46fea73291e2fddc0bbc72c9d0bf3afbe46e43b9
Tiago F. Santos <79919984+Tiagofsant@users.noreply.github.com>
2022-05-28 14:03:04 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,35 @@
+function caucula() {
+    let peso = document.getElementById("peso").value;
+    console.log(peso);
+    peso = Number(peso);
+    let agua = peso * 35;
+    console.log(agua)
+
+    if (idade <= 17) {
+        peso = Number(peso);
+        let agua = peso * 40;
+        console.log(agua)
+    }
+
+    else if (idade >= 18 && idade <= 55) {
+        peso = Number(peso);
+        let agua = peso * 35;
+        console.log(agua)
+    }
+
+    else if (idade >= 56 && idade <= 65) {
+        peso = Number(peso);
+        let agua = peso * 35;
+        console.log(agua)
+    }
+
+
+    else if (idade >= 66) {
+        peso = Number(peso);
+        let agua = peso * 25;
+        console.log(agua)
+    }
+
+
+    
+}
